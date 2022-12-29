9cc3981b1cda52ca7027f29e6fd479e5ad94fb2a
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2022-12-30 06:00:59 +0900

    Add files via upload

---
+++ 조건문.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   int num = 8;
+   if (num == 10)
+      Console.WriteLine("입력된 값이 10 입니다.");
+    else if(num==9)
+      Console.WriteLine("입력된 값이 9입니다.");
+    else
+      Console.WriteLine("입력된 값이 9와 10이 아닙니다.");
+  }
+}
